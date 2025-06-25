0dfdb20efea2f9a93e6aa414869ba17856531f7c
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-25 21:03:56 +0800

    Create CWE-78_example.c

---
+++ CWE-78_example.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void list_dir() {
+    char cmd[256] = "ls ";
+    char dir[200];
+    printf("Which dir to list? ");
+    fgets(dir, sizeof(dir), stdin);
+    dir[strcspn(dir, "\n")] = 0;
+    strcat(cmd, dir); // CWE‑78：未過濾 input，可能 “; rm -rf /”
+    system(cmd);
+}
+
+int main() {
+    list_dir();
+    return 0;
+}
