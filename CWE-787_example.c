cca0bf0315ad19b53b90871971b5c7c91b182335
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 21:33:24 +0800

    Update CWE-787_example.c

--- CWE-787_example.c
+++ CWE-787_example.c
@@ -6,7 +6,7 @@ void record_name() {
     char *buf = malloc(20);
     if (!buf) return;
     printf("Enter your name (<50 chars): ");
-    fgets(buf, 100, stdin); // CWE‑787: 讀入超過 buf 可用大小
+    fgets(buf, 100, stdin); 
     buf[strcspn(buf, "\n")] = 0;
     printf("Recorded name: %s\n", buf);
     free(buf);
