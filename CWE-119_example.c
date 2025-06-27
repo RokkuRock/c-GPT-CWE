f7008a80632bf1897e4c7fc5cccc211f866a0abd
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-27 10:30:15 +0800

    Update CWE-119_example.c

--- CWE-119_example.c
+++ CWE-119_example.c
@@ -1,11 +1,10 @@
-// cwe119_buffer_overflow.c
 #include <stdio.h>
 #include <string.h>
 
 void add_comment() {
     char comment[50];
     printf("Leave a comment: ");
-    fgets(comment, 512, stdin);  // CWE-119: 超過緩衝區長度
+    fgets(comment, 512, stdin);  
     printf("Your comment: %s\n", comment);
 }
 
