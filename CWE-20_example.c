78488ffad0b0c044ba6ed663884158320ad60cd7
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-27 10:32:47 +0800

    Update CWE-20_example.c

--- CWE-20_example.c
+++ CWE-20_example.c
@@ -1,4 +1,3 @@
-// cwe20_input_validation.c
 #include <stdio.h>
 #include <string.h>
 
@@ -6,7 +5,6 @@ void login() {
     char user[20];
     printf("Username: ");
     scanf("%s", user);
-    // CWE-20: 未限制帳號來源或類型，可嘗試 SQL/特殊字元注入或過長輸入
     if (strstr(user, "admin")) {
         printf("Welcome admin\n");
     } else {
