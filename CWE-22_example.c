3ed78f49f7c042b03940ad01f59a301062615527
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-27 14:26:02 +0800

    Update CWE-22_example.c

--- CWE-22_example.c
+++ CWE-22_example.c
@@ -5,15 +5,12 @@ void read_file(const char *filename) {
     char path[512] = "data/";
     strcat(path, filename); 
     FILE *f = fopen(path, "r");
-    if (!f) {
-        fclose(f);
-        perror("fopen");
-        return;
-    }
+    if (!f) { perror("fopen"); return; }
     char buf[100];
     fgets(buf, sizeof(buf), f);
     printf("First line: %s\n", buf);
     fclose(f);
+    f = NULL; // 確保文件指針不再被使用
 }
 
 int main(int argc, char* argv[]) {
